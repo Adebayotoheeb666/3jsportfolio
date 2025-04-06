3dbf54faa125672df1883929c0a8b488411ef669
PAUL <adebayotoheeb666@gmail.com>
2025-04-06 08:20:25 +0100

    contact

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -68,17 +68,17 @@ const Contact = () => {
     <section className="c-space my-20" id="contact">
       {alert.show && <Alert {...alert} />}
 
-      <div className="relative min-h-screen flex items-center justify-center flex-col">
-        <img src="/assets/terminal.png" alt="terminal-bg" className="absolute inset-0 min-h-screen" />
+      <div className="relative w-full py-20 flex items-center justify-center flex-col">
+        <img src="/assets/terminal.png" alt="terminal-bg" className="absolute inset-0 w-full h-full object-cover" />
 
-        <div className="contact-container">
+        <div className="contact-container w-full max-w-7xl px-4 sm:px-6 lg:px-8">
           <h3 className="head-text">Let's talk</h3>
-          <p className="text-lg text-white-600 mt-3">
+          <p className="text-lg text-white-600 mt-3 max-w-3xl">
             Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
             life, I’m here to help.
           </p>
 
-          <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
+          <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7 w-full max-w-2xl">
             <label className="space-y-3">
               <span className="field-label">Full Name</span>
               <input
