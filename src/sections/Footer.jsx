1ebdb5708ecf39e58f69dc033af426dd8ddfdffa
PAUL <adebayotoheeb666@gmail.com>
2025-04-06 07:33:08 +0100

    footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,15 +8,15 @@ const Footer = () => {
       </div>
 
       <div className="flex gap-3">
-        <div className="social-icon">
-          <img src="/assets/github.svg" alt="https://vercel.com/adebayotoheeb666s-projects/" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="https://x.com/splusplus111" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
-          <img src="/assets/instagram.svg" alt="https://www.instagram.com/splusplus111/" className="w-1/2 h-1/2" />
-        </div>
+        <a href="https://github.com/Adebayotoheeb666/" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <img src="/assets/github.svg" alt="Github" className="w-1/2 h-1/2" />
+        </a>
+        <a href="https://x.com/splusplus111" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <img src="/assets/twitter.svg" alt="Twitter" className="w-1/2 h-1/2" />
+        </a>
+        <a href="https://www.instagram.com/splusplus111/" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <img src="/assets/instagram.svg" alt="Instagram" className="w-1/2 h-1/2" />
+        </a>
       </div>
 
       <p className="text-white-500">© 2024 Adebayo Toheeb. All rights reserved.</p>
