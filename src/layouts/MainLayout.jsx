a17548de87bf6f8a9fb108f9fefdbad39646039f
PAUL <adebayotoheeb666@gmail.com>
2025-04-06 10:10:07 +0100

    new update

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../sections/Navbar';
 import Footer from '../sections/Footer';
 
 const MainLayout = () => {
   return (
     <main className="max-w-7xl mx-auto relative">
+      <Navbar />
       <Outlet />
       <Footer />
     </main>
