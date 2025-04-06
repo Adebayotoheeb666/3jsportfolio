49ae6e1c0d75f72dd3de0691c58d8ef9a62d66a2
PAUL <adebayotoheeb666@gmail.com>
2025-04-06 12:21:01 +0100

    new new

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+import Hero from './sections/Hero';
+import About from './sections/About';
+import Projects from './sections/Projects';
+import Contact from './sections/Contact';
+import Footer from './sections/Footer';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </Router>
+    <MainLayout>
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </MainLayout>
   );
 };
 
